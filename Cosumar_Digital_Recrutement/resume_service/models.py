c096f74707957a6e625289c5e2b2272c4d1bd17b
BABAYAGA-5 <othmane2rabat@gmail.com>
2025-08-12 23:54:51 +0100

    Changed architecture

--- Cosumar_Digital_Recrutement/resume_service/models.py
+++ Cosumar_Digital_Recrutement/resume_service/models.py
@@ -4,53 +4,71 @@ from datetime import datetime
 
 from numpy import extract
 
-class Candidat(models.Model):
+class Stagiaire(models.Model):
     id = models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    telephone = models.CharField(max_length=20, blank=True)
+    nom = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    num_tel = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    date_naissance = models.DateField(null=True, blank=True)
+    num_cin = models.CharField(max_length=8, unique=True, blank=True, null=True)
     date_creation = models.DateTimeField(auto_now_add=True)
+    deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.ForeignKey('auth_service.Utilisateur', on_delete=models.SET_NULL, null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.prenom} {self.nom}"
-    
-class Domaine(models.Model):
+class Sujet(models.Model):
     id = models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=100, unique=True)
-    keywords = models.JSONField(default=list)
-    date_creation = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.nom
-
-class Poste(models.Model):
-    id = models.AutoField(primary_key=True)
-    titre = models.CharField(max_length=100, unique=True)
+    titre = models.CharField(max_length=100)
     description = models.TextField()
-    date_publication = models.DateTimeField(auto_now_add=True)
-    date_expiration = models.DateTimeField(null=True, blank=True, default=datetime.now() + timedelta(days=30))
-    statut = models.CharField(max_length=50, choices=[('ouvert', 'Ouvert'), ('ferme', 'Fermé')], default='ouvert')
-    domaine = models.ForeignKey(Domaine, on_delete=models.CASCADE, related_name='poste', null=True, blank=True)
-
-    keywords = models.JSONField(default=list, blank=True)
+    deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.ForeignKey('auth_service.Utilisateur', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.titre
-    
-class Candidature(models.Model):
+
+class Stage(models.Model):
     id = models.AutoField(primary_key=True)
-    candidat = models.ForeignKey(Candidat, on_delete=models.CASCADE, related_name='candidatures')
-    poste = models.ForeignKey(Poste, on_delete=models.CASCADE, related_name='candidatures')
+    stagiaire = models.ForeignKey(Stagiaire, on_delete=models.CASCADE, related_name='candidatures')
+    nature = models.CharField(max_length=50, choices=[
+        ('stage', 'Stage'),
+        ('pfe', 'PFE'),
+        ('alternance', 'Alternance'),
+    ])
+    sujet = models.ForeignKey(Sujet, on_delete=models.CASCADE, related_name='candidatures')
     date_candidature = models.DateTimeField(auto_now_add=True)
-    statut = models.CharField(max_length=50, choices=[
-        ('en_attente', 'En attente'),
-        ('accepte', 'Accepté'),
-        ('refuse', 'Refusé')
-    ], default='en_attente')
-    cv = models.BinaryField(blank=False, null=False)
-    extracted_data = models.JSONField(default=dict, blank=True)
+    statut = models.CharField(
+        max_length=50,
+        choices=[
+            ('en_attente', 'En attente'),
+            ('accepte', 'Accepté'),
+            ('refuse', 'Refusé'),
+            ('en_attente_depot_dossier', 'En attente de dépôt de dossier'),
+            ('expire', 'Expiré'),
+            ('en_attente_visite_medicale', 'En attente de visite médicale'),
+            ('en_attente_signature_de_l_encadrant', 'En attente de signature de l\'encadrant'),
+            ('en_attente_signature_du_responsable_RH', 'En attente de signature du responsable RH'),
+            ('en_attente_signature_du_stagiaire', 'En attente de signature du stagiaire'),
+            ('stage_en_cours', 'Stage en cours')
+        ],
+        default='en_attente'
+    )
+    convention = models.BinaryField(null=True, blank=True)
+    assurance = models.BinaryField(null=True, blank=True)
+    lettre_motivation = models.BinaryField(null=True, blank=True)
+    cv = models.BinaryField(null=True, blank=True)
+    cin = models.BinaryField(null=True, blank=True)
+
+    def check_documents_and_expire(self):
+        if self.statut == 'accepte':
+            if not self.cv or not self.cin or not self.convention or not self.assurance:
+                self.statut = 'expire'
+                self.save()
 
     def __str__(self):
-        return f"Candidature de {self.candidat.prenom} {self.candidat.nom} pour le poste de {self.poste}"
+        return f"Candidature de {self.candidat.prenom} {self.candidat.nom} pour le sujet de {self.sujet.titre}"
 
+
+class Meta:
+    verbose_name = "Candidature"
